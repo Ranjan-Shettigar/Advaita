26a6133d9956e7f91b796ada3ea3f423b0db4606
Ranjan Shettigar <158475083+Student408@users.noreply.github.com>
2024-08-31 17:44:59 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,15 +7,11 @@ $dbname = "college_fest";
 
 
 
-define('GMAIL_USERNAME', 'reqres.team@gmail.com');
-define('GMAIL_PASSWORD', 'ecel xxwi dprr bhjd');
-define('GMAIL_SENDER_EMAIL', 'reqres.team@gmail.com');
-
 
 // Gmail credentials
-// define('GMAIL_USERNAME', 'yur@gmail.com');
-// define('GMAIL_PASSWORD', 'scbv dsgq tuai xaos');
-// define('GMAIL_SENDER_EMAIL', 'yur@gmail.com');
+define('GMAIL_USERNAME', 'yur@gmail.com');
+define('GMAIL_PASSWORD', 'scbv dsgq tuai xaos');
+define('GMAIL_SENDER_EMAIL', 'yur@gmail.com');
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
